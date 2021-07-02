f825fd34264e5c054c62301c77187a808bf2fc59
Vuong Dang <vuongdang@chidoanh.com>
2021-07-02 10:01:37 +0700

    update

---
+++ static/js/20.8fb9b466.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpaaa=this.webpackJsonpaaa||[]).push([[20],{631:function(t,e,n){"use strict";n.r(e),n.d(e,"amplify_container",(function(){return c}));var i=n(11),c=function(){function t(t){Object(i.k)(this,t)}return t.prototype.render=function(){return Object(i.i)(i.b,null,Object(i.i)("slot",null))},t}();c.style=":host{display:-ms-flexbox;display:flex;-ms-flex-align:center;align-items:center;-ms-flex-pack:center;justify-content:center;height:100vh}"}}]);
+//# sourceMappingURL=20.8fb9b466.chunk.js.map
